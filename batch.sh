d61080618e4a307def637af0c4ad244abe16e926
Hyungro Lee <hroe.lee@gmail.com>
2017-01-29 21:50:04 +0000

    sample stats

--- batch.sh
+++ batch.sh
@@ -7,14 +7,18 @@ ubuntu_4=10.0.10.94,10.0.10.92,10.0.10.99,10.0.10.93
 ubuntu_1_n=10.0.10.91
 ubuntu_4_n=10.0.10.95,10.0.10.97,10.0.10.96,10.0.10.98
 
-for i in ${ubuntu_4//,/ }
+ubuntu_8=
+ubuntu_8_n=
+#for i in ${ubuntu_4//,/ }
+for i in `echo 10.0.10.{137..144}`
 do
 msg='Acquire::http::proxy \"http://10.0.10.72:3142\";'
 ssh -o StrictHostKeyChecking=no ubuntu@$i "echo \"echo '"$msg"' > /etc/apt/apt.conf.d/02proxy\" > ssh_cmd.sh;sudo bash ssh_cmd.sh"
 python cache.py apt.install.from.docker $i ubuntu &
 done
 
-for i in ${ubuntu_4_n//,/ }
+#for i in ${ubuntu_4_n//,/ }
+for i in `echo 10.0.10.{145..152}`
 do
 python cache.py apt.install.from.docker $i ubuntu &
 done
--- elapsed.py
+++ elapsed.py
@@ -3,15 +3,32 @@ import sys
 import json
 from pprint import pprint
 
-with open(sys.argv[1]) as f:
-	lines=f.read()
-	data = json.loads(lines)
+def mean(numbers):
+    return float(sum(numbers)) / max(len(numbers), 1)
 
-for k, v in data.iteritems():
-	stdout = v['stdout']
-	m = re.search('Fetched.*', stdout)
-	if m:
-		pprint(m.group())
-	else:
-		pass#print k
-	
+if __name__ == "__main__":
+	with open(sys.argv[1]) as f:
+		lines=f.read()
+		data = json.loads(lines)
+
+	tot = 0
+	for k, v in data.iteritems():
+		stdout = v['stdout']
+		m = re.search('Fetched.*', stdout)
+		if m:
+			fetched=m.group()
+			m2 = re.search('\(.*\)', fetched)
+			tmp = m2.group()[1:-1]
+			sp_unit = tmp.split()
+			s = sp_unit[0].replace(",","")
+			m_k_b = sp_unit[1]
+			if s == "0":
+				continue
+			if m_k_b[0] == "k":
+				s = float(s) / 1024
+			tot = mean([tot, float(s)])
+		else:
+			pass#print k
+
+	print tot
+		
